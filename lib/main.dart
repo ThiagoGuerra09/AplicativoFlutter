00c2bd53dc6cce098985a69891487ccbcc4c7bdd
ThiagoGuerra09 <thiagogwf@gmail.com>
2022-10-28 14:53:34 -0300

    startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/item.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,109 +9,105 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Todo app',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+  var items = <Item>[];
 
-  void _incrementCounter() {
+  void add() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      items.add(
+        Item(
+          title: newTaskCtrl.text,
+          done: false,
+        ),
+      ); // adiciona novos itens
+      newTaskCtrl.text = "";
     });
   }
 
+  void remove(int index) {
+    setState(() {
+      items.removeAt(index);
+    });
+  }
+
+
+  load() async{
+    
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+              labelText: "Digite o nome para sua lista",
+              labelStyle: TextStyle(color: Colors.brown, fontSize: 18)),
         ),
+        leading: Text('Versão beta'),
+        actions: <Widget>[Icon(Icons.menu)],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => add(),
+        child: Icon(Icons.add),
+        backgroundColor: Color.fromRGBO(79, 62, 13, 0.932),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title.toString()),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  // função do stf que muda o estado dos parametros passados, serve para quando vamos atualizar uma posição
+                  item.done = value;
+                });
+              },
+            ),
+            key: Key(item.title.toString()),
+            background: Container(
+              color: Colors.red.withOpacity(0.2),
+              child: Text("Excluir"),
+            ),
+            onDismissed: (direction) {
+              print(direction);
+              remove(index);
+            },
+          );
+        },
+      ),
     );
   }
 }
+//  Container(
+//   child: Center(
+//     child: Text(
+//       'ola mundo',
+//     ),
+//   ),
+// ),
